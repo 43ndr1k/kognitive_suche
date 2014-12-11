2b6dbce0f305770d75d927eb3d1a84f3495dec02
tdelle1 <tim.delle@stud.htwk-leipzig.de>
2014-12-11 15:02:43 +0100

    [ADD] Example Dummy methods

--- kognitive_suche/src/controller/Controller.java
+++ kognitive_suche/src/controller/Controller.java
@@ -3,8 +3,39 @@ package controller;
 /**
  * Dummy Controller<br>
  * TODO: Create Dummy methods to create a simple workflow
+ * 
+ * The following methods are suggestions only
  * @author tim.delle@stud.htwk-leipzig.de
  */
 public class Controller {
 
+	/* CONSTANTS */
+	
+	/* VARIABLES */
+	
+	/* CONSTRUCTOR */
+	
+	/* METHODS */
+	
+	
+	private Object evaluateGUI(Object pObject){
+		return null;
+	}
+	
+	private Object queryFaroo(Object pObject){
+		return null;
+	}
+	
+	private Object queryPdfBox(Object pObject){
+		return null;
+	}
+	
+	private Object startSimpleAlgorithmn(Object pObject){
+		return null;
+	}
+	
+	private Object startComplexAlgorithmn(Object pObject){
+		return null;
+	}
+	
 }
