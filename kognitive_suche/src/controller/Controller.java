789e62ad85af1d35564ea2e657078f11c3825d3e
tdelle1 <tim.delle@stud.htwk-leipzig.de>
2014-12-11 14:51:16 +0100

    [ADD] description

--- kognitive_suche/src/controller/Controller.java
+++ kognitive_suche/src/controller/Controller.java
@@ -1,5 +1,10 @@
 package controller;
 
+/**
+ * Dummy Controller<br>
+ * TODO: Create Dummy methods to create a simple workflow
+ * @author tim.delle@stud.htwk-leipzig.de
+ */
 public class Controller {
 
 }
