e7122f9700568596129391a632272b50f8440ac5
cschmid5 <christian.schmidt.1@stud.htwk-leipzig.de>
2014-12-11 21:40:02 +0100

    Paar Ideen zum Controller

--- kognitive_suche/src/controller/Controller.java
+++ kognitive_suche/src/controller/Controller.java
@@ -1,5 +1,13 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+import Faroo.API;
+import Faroo.ConfigFileManagement;
 /**
  * Dummy Controller<br>
  * TODO: Create Dummy methods to create a simple workflow
@@ -10,13 +18,40 @@ package controller;
 public class Controller {
 
 	/* CONSTANTS */
-	
+	private int test1 = 1;
 	/* VARIABLES */
+	private String Suchstring; // Die Eingabe an Farroo und pdfbox
+	private String Config; //Erstmal nur zum Test, wird entweder 1 Array oder mehrere Variablen
 	
 	/* CONSTRUCTOR */
+	public Controller(String Suchstring, String Config){//Nen Test ob wir Netz haben wäre vlt. noch nützlich
+		this.Suchstring = Suchstring;
+		this.Config = Config;
+		
+	}
+	
+	
+	/* CODE - Auswahl Farroo, PDFBox, Einfacher Suchalg., etc. */
+	/*switch (test1) {
+	
+		case1://Farroo
+			API api = new API(config.getKey(Suchstring));
+			break;
+		case2://Pdfbox
+			break;
+		case3://
+			break;
+		case4:
+			break; Schlechte IDee*/
+		
+	}
+	
 	
 	/* METHODS */
-	
+	private void getconfig(){
+		
+	}
+	//------------------------------------------------
 	
 	private Object evaluateGUI(Object pObject){
 		return null;
