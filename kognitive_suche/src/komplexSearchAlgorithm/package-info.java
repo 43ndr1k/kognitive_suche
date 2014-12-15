455300be1d6a3f5efcd107bad84dc316611ea548
tlenz <tobais.lenz@stud.htwk-leipzig.de>
2014-12-15 16:14:00 +0100

    Algorithmus eingefuegt, kommentarlos

---
+++ kognitive_suche/src/komplexSearchAlgorithm/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author tlenz
+ *
+ */
+package komplexSearchAlgorithm;
---
+++ kognitive_suche/src/komplexSearchAlgorithm/suchobjekt.java
@@ -0,0 +1,31 @@
+package komplexSearchAlgorithm;
+
+public class suchobjekt {
+
+	private String link, titel, searchtext;
+
+	/**
+	 * Beispielhafte Implementation des Objekts Suchergebnis
+	 * Hinweis: in der Zeichenkette searchtext stehen die Begriffe, welche in dem Suchergebnis um den Suchtext herum gefunden wurden; getrennt durch Leerzeichen
+	 * @param link
+	 * @param titel
+	 * @param searchtext
+	 */
+	public suchobjekt(String link, String titel, String searchtext){
+		this.link = link;
+		this.titel = titel;
+		this.searchtext = searchtext;
+		
+	}
+	public String getlink() {
+		return link;
+	}
+	public String gettitel() {
+		return titel;
+	}
+	public String getsearchtext() {
+		return searchtext;
+	}
+	
+	
+}
