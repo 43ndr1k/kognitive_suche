3cdf5619c56430293c97c2e0e60a0b713a1a209c
fschwar <franz.schwarzer@stud.htwk-leipzig.de>
2014-12-18 05:19:16 +0100

    Franz Schwarzer Meta Keys mit Threads zur Zeitersparnis . Hoffe das ist dann jetzt auch wirklich im Git :D

---
+++ kognitive_suche/src/komplexeSuche/Statics.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+//@author Franz Schwarzer
+
+public class  Statics {
+	
+	static String[][] urlkeys=new String[10][];
+	//static ArrayList<String[]> urlkeys=new ArrayList<String[]>();
+	static String[] keys;
+
+}
---
+++ kognitive_suche/src/komplexeSuche/allMeta.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+//@author Franz Schwarzer
+
+public class allMeta {
+
+Tag_Meta[] meta=new Tag_Meta[9];
+ArrayList<String[]> keys;
+
+allMeta(ArrayList<String> urls){
+	
+	for(int i=0;i<9;i++){
+		meta[i]=new Tag_Meta(urls.get(i),i);
+		meta[i].start();
+		
+	}
+
+	while(ready()==false){
+		
+	}
+	//System.out.println(Statics.keys[0]);
+	
+/*	for(int i=0;i<9;i++){
+		keys.add(meta[i].keys);
+	}*/
+	
+
+}
+
+public boolean ready(){
+	for(int i=0;i<9;i++){
+		if(meta[i].isAlive()==false){return false;}
+	}
+	return true;
+	
+}
+	
+}
