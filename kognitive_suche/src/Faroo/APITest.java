f5c79c0d6e335b4bf5500c4355bb6ab7f0e7bb1e
hsawade <hendrik.sawade@stud.htwk-leipzig.de>
2015-01-07 17:22:41 +0100

    [testing] faroo testen

---
+++ kognitive_suche/src/Faroo/APITest.java
@@ -0,0 +1,71 @@
+package Faroo;
+
+import static org.junit.Assert.*;
+
+public class APITest {
+
+    @org.junit.Test
+    public void testQuery() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery2() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery3() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery4() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery5() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery6() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery7() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery8() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery9() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery10() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testQuery11() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetResult() throws Exception {
+
+    }
+}
