17ba154bad93d6c15b84c038115fb9bd4db6aed6
cschmid5 <christian.schmidt.1@stud.htwk-leipzig.de>
2015-03-19 19:24:19 +0100

    Impementierung von Buttons für die Suchoptionen in der GUI - für Sadik gleich noch die Sprachbuttons mit erstellt

--- kognitive_suche/src/GUI/GUI.java
+++ kognitive_suche/src/GUI/GUI.java
@@ -8,12 +8,14 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -27,6 +29,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import komplexeSuche.TagObject;
 import komplexeSuche.TagObjectList;
@@ -223,13 +226,31 @@ public class GUI extends Application{
 		pane1.getChildren().clear();
 		HBox hbox1 = new HBox();//horizontale Box für Suchleiste und Buttons
 		HBox hbox2 = new HBox();//schliessen box
+		HBox hbox3 = new HBox();//Horizontale Box für Buttons von vbox2 und vbox3
 		VBox vbox1 = new VBox();//vertikale Box für Logo, hbox2
+		VBox vbox2 = new VBox();//Vertikale Box für Sprachauswahl Buttons
+		VBox vbox3 = new VBox();//Vertikale Box für Suchartauswahl Buttons
+		final String[] SelectedLanguage = {"de"}; // Variable für Sprachauswahl
+	    final String[] Selectedsrc = {"web"}; // Variable für Suchartauswahl
 		pane1.setStyle("-fx-background-color: #FFF;");
 		pane1.setCenter(vbox1);
 		pane1.setBottom(hbox2);//schliessen
 
 		hbox1.setAlignment(Pos.CENTER);
 		hbox2.setAlignment(Pos.BOTTOM_RIGHT);//Rechte ecke postionsbestimmung closebox
+		
+		//vbox2.setAlignment(arg0);
+		//vbox3.setAlignment(arg0);
+		hbox3.setAlignment(Pos.BOTTOM_CENTER);
+		
+		hbox3.setPadding(new Insets(-50));
+		hbox3.setSpacing(20);
+		
+		vbox2.setPadding(new Insets(10));
+		vbox2.setSpacing(10);
+		
+		vbox3.setPadding (new Insets(10));
+		vbox3.setSpacing(10);
 		
 		hbox1.setPadding(new Insets(15,30,15,30));				/*Bestimmt den Abstand vom Rand nach Innen*/
 		hbox1.setSpacing(20);									/*Bestimmt den Abstand der Elemente voneinander*/
@@ -291,9 +312,87 @@ public class GUI extends Application{
 
 		});
 		
-		hbox1.getChildren().addAll(suchleiste,sucheF,sucheP);
-        vbox1.getChildren().addAll(homebutton(), hbox1);
+		
+        /**
+    	 * @author Christian Schmidt
+    	 */
+      //------------------------------------------------------------------------------------
+
+		Label label = new Label("Farroo Suchoptionen");
+        label.setFont(Font.font("Arial", 14));
+        HBox schrift = new HBox(label);
+        schrift.setAlignment(Pos.CENTER);
+        schrift.setPadding(new Insets(-15,15,15,15));
+        schrift.setSpacing(10);
+		
+       final Button[] btnlanguage = new Button[2];
+       btnlanguage[0] = new Button("de");
+       btnlanguage[0].setText("German");
+       btnlanguage[0].setStyle("-fx-background-color: #00B2EE;");
+       btnlanguage[0].setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent event) {
+               btnlanguage[0].setStyle("-fx-background-color: #00B2EE;");
+               btnlanguage[1].setStyle("-fx-background: #FFFFFF;");
+               SelectedLanguage[0] = "de";
+
+           }
+       });
+       btnlanguage[1] = new Button("en");
+       btnlanguage[1].setText("English");
+       btnlanguage[1].setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent event) {
+               btnlanguage[1].setStyle("-fx-background-color: #00B2EE;");
+               btnlanguage[0].setStyle("-fx-background: #FFFFFF;");
+               SelectedLanguage[0] = "en";
+           }
+       });
+
+       final Button[] btnsrc = new Button[3];
+       btnsrc[0] = new Button("web");
+       btnsrc[0].setText("Web");
+       btnsrc[0].setStyle("-fx-background-color: #00B2EE;");
+       btnsrc[0].setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent event) {
+               btnsrc[0].setStyle("-fx-background-color: #00B2EE;");
+               btnsrc[1].setStyle("-fx-background: #FFFFFF;");
+               btnsrc[2].setStyle("-fx-background: #FFFFFF;");
+               Selectedsrc[0] = "web";
+           }
+       });
+       btnsrc[1] = new Button("news");
+       btnsrc[1].setText("News");
+       btnsrc[1].setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent event) {
+               btnsrc[1].setStyle("-fx-background-color: #00B2EE;");
+               btnsrc[0].setStyle("-fx-background: #FFFFFF;");
+               btnsrc[2].setStyle("-fx-background: #FFFFFF;");
+               Selectedsrc[0] = "news";
+           }
+       });
+       btnsrc[2] = new Button("topics");
+       btnsrc[2].setText("Headlines");
+       btnsrc[2].setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent event) {
+               btnsrc[2].setStyle("-fx-background-color: #00B2EE;");
+               btnsrc[1].setStyle("-fx-background: #FFFFFF;");
+               btnsrc[0].setStyle("-fx-background: #FFFFFF;");
+               Selectedsrc[0] = "topics";
+           }
+       });
+   	
+   	//--------------------------------------------------
+        vbox2.getChildren().addAll(btnlanguage[0],btnlanguage[1]);
+		vbox3.getChildren().addAll(btnsrc[0],btnsrc[1],btnsrc[2]);
+		hbox3.getChildren().addAll(vbox2,vbox3);
+        hbox1.getChildren().addAll(suchleiste,sucheF,sucheP);
+        vbox1.getChildren().addAll(homebutton(), hbox1, schrift, hbox3);
         hbox2.getChildren().addAll(close);
+        
 		return start;
 	}
 	
