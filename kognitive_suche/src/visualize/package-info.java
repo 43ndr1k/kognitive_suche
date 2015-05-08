aa5da8987569bc934e4036d8e360d7996d9d1c53
vnaumchy <vadzim.naumchyk@stud.htwk-leipzig.de>
2015-05-08 15:57:05 +0200

    [changed] unwichtig

---
+++ kognitive_suche/src/visualize/package-info.java
@@ -0,0 +1,12 @@
+//TODO Das Kommentar zu dem Package bitte ändern
+/**
+ * Der erste Satz als kurze Beschreibung für das Package.
+ * Eine ausführlichere Beschreibung für das Package {@code visualize}.
+ * <p>
+ * Eventuell zweiter Absatz der Beschreibung.
+ *
+ * @author Autor1
+ * @author Autor2
+ * @version 1.0
+ */
+package visualize;
