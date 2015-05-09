3243b3bfb7d46f9acf143cb3061e15159ef76cdc
maokai <tim.delle@googlemail.com>
2015-05-09 14:30:37 +0200

    [ADD] GUI Instance in Pattern

---
+++ kognitive_suche/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+import gui.GUI;
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+        Application.launch(GUI.class, args);
+	}
+
+}
