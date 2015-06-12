97794488e6eed2c6198528a7ed53bf83163dd7e1
sulusan <sadik.ulusan@stud.htwk-leipzig.de>
2015-06-12 23:46:59 +0200

    Testfaelle SearchHistory und TxtReader durchgefuehrt, SearchHistory ueberarbeitet.

---
+++ kognitive_suche/src/main/java/de/leipzig/htwk/tests/SearchHistoryTest.java
@@ -0,0 +1,51 @@
+package de.leipzig.htwk.tests;
+
+import static org.junit.Assert.assertEquals;
+/**
+ * 
+ * 
+ * 
+ * @author Sadik Ulusan
+ * 
+ *
+ */
+
+import java.util.ArrayList;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.leipzig.htwk.search.history.HistoryObject;
+import de.leipzig.htwk.search.history.SearchHistory;
+
+public class SearchHistoryTest {
+
+  static SearchHistory testHistory;
+
+  @BeforeClass
+  public static void prepare() {
+    testHistory = new SearchHistory();
+
+  }
+
+  @Test
+  public void testHistory() {
+    int length;
+    HistoryObject foo;
+   
+    ArrayList<HistoryObject> searchHistory;
+    searchHistory = testHistory.getSearches();
+    length = searchHistory.size();
+    testHistory.addSearch("JUnit Test");
+    assertEquals(length + 1, searchHistory.size());
+
+    searchHistory = testHistory.getSearches();
+    foo = searchHistory.get(searchHistory.size() - 1); // Foo = das letzte Element der Liste
+    assertEquals("JUnit Test", foo.searchWord);
+
+    testHistory.historyData.remove(testHistory.historyData.size() - 1); // letzter Testfall wird aus
+                                                                        // der searchHistory
+                                                                        // geloescht
+    testHistory.saveSearch();
+  }
+}
